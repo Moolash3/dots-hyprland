2953d237ccb981ffa7e331f801749a6998a8dd7c
moolash3 <adam.moolash@gmail.com>
2025-10-05 12:33:05 -0700

    idk maybe

--- dots/.config/fish/config.fish
+++ dots/.config/fish/config.fish
@@ -3,20 +3,6 @@ if status is-interactive
     # No greeting
     set fish_greeting
 
-    # Use starship
-    function starship_transient_prompt_func
-        starship module character
-    end
-    if test "$TERM" != "linux"
-        starship init fish | source
-        enable_transience
-    end
-    
-    # Colors
-    if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-        cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-    end
-
     # Aliases
     # kitty doesn't clear properly so we need to do this weird printing
     alias clear "printf '\033[2J\033[3J\033[1;1H'"
